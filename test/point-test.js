75eb7a549823e0d531b4137f46431c5b958ab511
Brant Wellman <BrantWellman@gmail.com>
2016-03-29 17:55:46 -0600

    Add testing for player and vectors

--- test/point-test.js
+++ test/point-test.js
@@ -1,7 +1,17 @@
-// const canvas = document.getElementById("game-canvas");
-// const ctx = canvas.getContext('2d');
+const chai = require('chai');
+const assert = chai.assert;
 
 const Player = require('../lib/player');
 const Point = require('../lib/point');
 const Vector = require('../lib/vector');
 const Edge = require('../lib/edge');
+
+
+// describe('Point', function(){
+//   context('returns new vector', function() {
+//     var
+//     it('should return coords that lie on the visual plane', function(){
+//
+//     });
+//   });
+// });
--- test/vector-test.js
+++ test/vector-test.js
@@ -7,7 +7,7 @@ const Vector = require('../lib/vector');
 const Edge = require('../lib/edge');
 
 describe('Vector', function(){
-  context('returns new vector', function() {
+  context('vector math', function() {
     var vector = new Vector(1,3,2);
     var vector2 = new Vector(-2,3,5);
     var location = new Vector(2,3,4);
@@ -23,7 +23,7 @@ describe('Vector', function(){
     });
 
     it('should return the dot product from 2 vectors', function() {
-      assert.equal(17, vector.dot(vector2))
+      assert.equal(17, vector.dot(vector2));
     });
 
     it('should return cross product from 2 vectors', function() {
@@ -33,6 +33,20 @@ describe('Vector', function(){
 
     it('should return the magnitude of length', function() {
       assert.equal(Math.sqrt(14), vector.mag());
+    });
+
+    it('should return the scalar projection', function() {
+      var vector3 = new Vector(1,0,0);
+      var vector4 = new Vector(0.5,0,0.5);
+      assert.equal(0.5, vector4.comp(vector3));
+
+      var vector5 = new Vector(6,6,0);
+      assert.equal((0.5*Math.sqrt(2)), vector3.comp(vector5));
+    });
+
+    it('should return the distance between two points', function() {
+      var location2 = new Vector(2,3,8)
+      assert.equal(4, location.dist(location2))
     });
   });
 });
