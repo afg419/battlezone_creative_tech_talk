c2042d6e06e95b7d4220b5838addde79d5d23b4d
Aaron Greenspan <afg419@gmail.com>
2016-04-01 17:07:02 -0600

    Finalizes tests with faces rendering in correct order

--- test/obstacle-test.js
+++ test/obstacle-test.js
@@ -1,3 +1,6 @@
+const chai = require('chai');
+const assert = chai.assert;
+
 const Obstacle = require('../lib/obstacle');
 const Vector = require('../lib/vector');
 const Player = require('../lib/player');
@@ -5,21 +8,17 @@ const LevelBuilder = require('../lib/level_builder');
 
 describe('obstacle functionality', function(){
   context('weird bug, why?', function() {
-    it('computes normal vector to plane wrt to origin and flips', function(){
-      // e[0] = new Edge(p1, p2)
-      // e[1] = new Edge(p2, p3)
-      // e[2] = new Edge(p3, p4)
-      // e[3] = new Edge(p4, p1)
-      // var origin = new Vector(0,-2,0)
-      // var diamond = new Face(e)
-      // var computed = diamond.normalVector(origin)
+    it('orders faces by distance to player', function(){
       var builder = new LevelBuilder()
-      var player = new Player(new Vector(160.257,-5.237,25), new Vector(0.074,0.997,0), 5);
+      var player = new Player(new Vector(0,0,0), new Vector(1,0,0), 5);
       var obs = new Obstacle(builder.squareBuilding(100,100,100,100,100,true), new Vector(150,150,50));
-      var computed = obs.visibleFacesTo(player);
-      debugger
+      var computed = obs.orderedFacesByDistTo(player).map(face => {
+        return face.distanceTo2(player)
+      });
 
-      assert(computed.equals(new Vector(0,1,0)))
+      assert(computed[0] >= computed[1])
+      assert(computed[1] >= computed[2])
+      assert(computed[2] >= computed[3])
     });
   });
 });
